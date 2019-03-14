b63c383a866f8add424add4247aef4c3d0d81710
bbakalov <bohdan.bakalov@everything5pounds.com>
2019-03-14 15:05:22 +0200

    was created tables: product_shop_stock, shop_list

---
+++ app/code/Bforward/PickUpProductFromShop/Setup/Schema.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Bforward\PickUpProductFromShop\Setup;
+
+interface Schema
+{
+    const PRODUCT_SHOP_STOCK_TABLE = 'product_shop_stock';
+    const SHOP_LIST_TABLE = 'shop_list';
+}
