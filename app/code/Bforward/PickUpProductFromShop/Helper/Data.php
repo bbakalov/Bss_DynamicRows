189e3789d431f81254e6c844723ef2ef6c35da8b
bbakalov <bohdan.bakalov@everything5pounds.com>
2019-03-19 17:03:19 +0200

    module configuration was added | shipping module was added

---
+++ app/code/Bforward/PickUpProductFromShop/Helper/Data.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Bforward\PickUpProductFromShop\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\ScopeInterface;
+
+class Data extends AbstractHelper
+{
+
+    const XML_PATH_HELLOWORLD = 'pickupproductfromshop/';
+
+    public function getConfigValue($field, $storeId = null)
+    {
+        return $this->scopeConfig->getValue(
+            $field, ScopeInterface::SCOPE_STORE, $storeId
+        );
+    }
+
+    public function getGeneralConfig($code, $storeId = null)
+    {
+        return $this->getConfigValue(self::XML_PATH_HELLOWORLD . 'general/' . $code, $storeId);
+    }
+}
